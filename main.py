1c7e0bf7c319be23ca94679c26a2e6003ed1d0d7
A-R-E-X-U-S <mstfaeybk@gmail.com>
2023-11-20 22:54:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print("AREXUS - Kalan Maaş Hesaplama")
+print("")
+ad = input("Lütfen adınızı giriniz: ")
+print("Merhaba " + ad + ".")
+print("")
+
+maas = float(input("Maaşınızı yazınız: "))
+vergi_orani = float(input("Vergi oranını yüzde olarak giriniz: ")) / 100
+
+kalan_maas = maas - (maas * vergi_orani)
+print("Tüm vergiler çıkıldığında size kalan maaş: " + str(kalan_maas))

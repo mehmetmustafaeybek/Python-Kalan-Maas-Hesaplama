e497e82f81671a1d767b122a6d3251406ab06ecd
Mehmet Mustafa Eybek <138300217+mehmetmustafaeybek@users.noreply.github.com>
2023-11-20 23:19:35 +0300

    main.py

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ vergi_orani = float(input("Vergi oranını yüzde olarak giriniz: ")) / 100
 
 kalan_maas = maas - (maas * vergi_orani)
 print("Tüm vergiler çıkıldığında size kalan maaş: " + str(kalan_maas))
+print("Bizi seçtiğiniz için teşekkürler.")
